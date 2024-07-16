3beb4ded6df1673962b49cb10847d0770078377c
felipepestanaa <felipe-pe27@hotmail.com>
2024-07-16 11:43:11 -0300

    add: module of agents create and dashboard

--- valorant-front-end/src/app/agents/agent-dashboard/agent-dashboard.component.ts
+++ valorant-front-end/src/app/agents/agent-dashboard/agent-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-agent-dashboard',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./agent-dashboard.component.css']
 })
 export class AgentDashboardComponent {
+
+
+  constructor(private router: Router){}
+
+  createAgent(){
+    this.router.navigateByUrl('agents/create');
+  }
 
 }
--- valorant-front-end/src/app/app-routing.module.ts
+++ valorant-front-end/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AgentDashboardComponent } from './agents/agent-dashboard/agent-dashboard.component';
 import { MapsDashboardComponent } from './maps/maps-dashboard/maps-dashboard.component';
 import { MatchDashboardComponent } from './match/match-dashboard/match-dashboard.component';
+import { AgentCreateComponent } from './agents/agent-create/agent-create.component';
 
 
 const routes: Routes = [
   { path: '', component: MatchDashboardComponent, pathMatch: 'full'},
   { path: 'agents', component: AgentDashboardComponent, },
+  { path: 'agents/create', component: AgentCreateComponent, },
   { path: 'maps', component: MapsDashboardComponent, },
 ];
 

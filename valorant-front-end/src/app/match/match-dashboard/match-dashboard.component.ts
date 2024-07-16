c5cb8b2dafd2734d2122b9f9f02f611c024c02eb
felipepestanaa <felipe-pe27@hotmail.com>
2024-07-16 15:57:49 -0300

    add: list of match

--- valorant-front-end/src/app/match/match-dashboard/match-dashboard.component.ts
+++ valorant-front-end/src/app/match/match-dashboard/match-dashboard.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class MatchDashboardComponent {
 
+
+  InformationAboutMatch(){
+    console.log("podemos sorrir!");
+  }
+
+  createNewMatch(){
+    
+  }
+
 }
--- valorant-front-end/src/app/match/match.module.ts
+++ valorant-front-end/src/app/match/match.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MapsCreateComponent } from './maps-create/maps-create.component';
-import { MapsDashboardComponent } from './maps-dashboard/maps-dashboard.component';
 import { MatchDashboardComponent } from './match-dashboard/match-dashboard.component';
 import { MatchCreateComponent } from './match-create/match-create.component';
 
@@ -9,8 +7,6 @@ import { MatchCreateComponent } from './match-create/match-create.component';
 
 @NgModule({
   declarations: [
-    MapsCreateComponent,
-    MapsDashboardComponent,
     MatchDashboardComponent,
     MatchCreateComponent
   ],

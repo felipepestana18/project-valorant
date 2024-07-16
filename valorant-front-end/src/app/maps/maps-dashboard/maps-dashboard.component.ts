f06394c749aed92230d24816a6aa58ee67c5d3c9
felipepestanaa <felipe-pe27@hotmail.com>
2024-07-16 12:38:01 -0300

    add: front-end maps

--- valorant-front-end/src/app/maps/maps-dashboard/maps-dashboard.component.ts
+++ valorant-front-end/src/app/maps/maps-dashboard/maps-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-maps-dashboard',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class MapsDashboardComponent {
 
+  constructor(private router: Router){}
+  
+  createMap(){
+    this.router.navigateByUrl('maps/create');
+  }
 }
--- valorant-front-end/src/app/maps/maps.module.ts
+++ valorant-front-end/src/app/maps/maps.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { MapsDashboardComponent } from './maps-dashboard/maps-dashboard.component';
 import { MapsCreateComponent } from './maps-create/maps-create.component';
 
@@ -8,12 +7,14 @@ import { MapsCreateComponent } from './maps-create/maps-create.component';
 
 @NgModule({
   declarations: [
-    DashboardComponent,
     MapsDashboardComponent,
     MapsCreateComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    MapsDashboardComponent
   ]
 })
 export class MapsModule { }

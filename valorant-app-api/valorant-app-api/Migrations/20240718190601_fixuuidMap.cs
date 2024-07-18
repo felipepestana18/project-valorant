578eda873e55cd92cadddb349d70a0a79dd8fa8d
felipepestanaa <felipe-pe27@hotmail.com>
2024-07-18 16:26:41 -0300

    fix: atributtes uid

---
+++ valorant-app-api/valorant-app-api/Migrations/20240718190601_fixuuidMap.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace valorant_app_api.Migrations
+{
+    public partial class fixuuidMap : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "uuid",
+                table: "Maps",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "uuid",
+                table: "Maps");
+        }
+    }
+}
